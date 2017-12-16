583022112524423af1aafb5e0c330328ca57cf37
karuna24s <karuna24s@gmail.com>
2017-12-16 01:22:23 -0500

    generate scaffold for cart and update migrations for item

--- db/migrate/20171216053025_create_items.rb
+++ db/migrate/20171216053025_create_items.rb
@@ -3,6 +3,7 @@ class CreateItems < ActiveRecord::Migration[5.0]
     create_table :items do |t|
       t.string :name
       t.integer :price
+      t.integer :cart_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216053025) do
+ActiveRecord::Schema.define(version: 20171216055016) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.integer  "price"
+    t.integer  "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
