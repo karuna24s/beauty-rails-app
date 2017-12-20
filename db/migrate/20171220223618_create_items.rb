abeb6741456368bed88241d9825aee8facf55fc9
karuna24s <karuna24s@gmail.com>
2017-12-20 17:46:20 -0500

    attempted to create a migration file but cannot create a table with items which is weird

--- db/migrate/20171220223618_create_items.rb
+++ db/migrate/20171220223618_create_items.rb
@@ -1,7 +1,10 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
-
+      t.string :name
+      t.decimal :price, precision: 12, scale: 3
+      t.boolean :active
+      
       t.timestamps
     end
   end
